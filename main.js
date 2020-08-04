7b9c10b6b4564239204c0294b76cb4a5feb51e15
Edward Ludwig <jllc0211@gmail.com>
2020-08-04 14:36:09 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const todayText = document.querySelector("#today");
+const nextTurn = document.querySelector("#next-turn");
+const car = document.querySelector("#car");
+
+// Create Car Class
+class Car {
+  constructor(model, brand, lastDigit) {
+    this.model = model;
+    this.brand = brand;
+    this.lastDigit = lastDigit;
+  }
+  get getLastDigit() {
+    return this.lastDigit;
+  }
+}
+
+// Change the day
+function changeDay() {
+  // get Day
+  let day = new Date(),
+    month = day.toLocaleString("default", { month: "long" }).toUpperCase(),
+    hour = day.getHours(),
+    today = day.getDate();
+
+  todayText.innerHTML = `${month} ${today}`;
+
+  setTimeout(changeDay, 1000);
+}
+
+function gasDay() {
+  let day = new Date(),
+    today = day.getDate();
+  let gasday = new Date(2020, 7, 5),
+    dayOnly = gasday.getDate();
+  if (dayOnly - today > 0) {
+    nextTurn.innerHTML = `Siguiente turno: ${dayOnly - today} día(s).`;
+  } else if (dayOnly - today === 0) {
+    nextTurn.textContent = "¡Hoy te toca!";
+    dayOnly += 6;
+  }
+
+  setTimeout(gasDay, 60000);
+}
+
+changeDay();
+gasDay();
